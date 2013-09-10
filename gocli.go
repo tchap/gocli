805ec9631a38e4138d000e5cea22f8fc53465689
Ondrej Kupka <ondra.cap@gmail.com>
2013-09-10 10:55:03 +0200

    Rewrite the whole thing

--- gocli.go
+++ gocli.go
@@ -24,11 +24,11 @@
 package gocli
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"text/template"
 )
 
 type Command struct {
@@ -36,11 +36,23 @@ type Command struct {
 	Short     string
 	Long      string
 
-	Flags flag.FlagSet
-	Args  []string
+	Flags     *flag.FlagSet
 
+	// All subcommands registered. Although it can be accessed directly,
+	// it is better to use MustRegisterSubcommand to append new subcommands,
+	// also because it automatically adds help subcommands.
+	Subcmds []*Command
+
+	// Remember the parent command during flag parsing so that we can generate
+	// the help output from the whole chain of subcommands.
 	parent  *Command
-	subcmds []*Command
+}
+
+func NewCommand() *Command {
+	var cmd Command
+	cmd.Flags = flag.NewFlagSet("", flag.ExitOnError)
+	cmd.Flags.Usage = cmd.Usage
+	return &cmd
 }
 
 func (cmd *Command) Name() string {
@@ -51,102 +63,69 @@ func (cmd *Command) Name() string {
 	return name
 }
 
-func (cmd *Command) Usage(msg ...string) {
-	// Print message if supplied
-	if len(msg) == 1 {
-		fmt.Fprintf(os.Stderr, "%s\n\n", msg[0])
-	}
+var usageTemplate = `COMMAND:
+    {{.Name}} - {{.Short}}
+USAGE:
+    {{.UsageLine}}
+{{with $opts := call .OptionsString}}
+OPTIONS:
+    {{print $opts}}
+{{end}}
+DESCRIPTION:
+    {{.Long}}
+{{with .Subcmds}}
+SUBCOMMANDS:
+    {{range .}}{{.Name}}{{end}}
+{{end}}
+`
 
-	// Print usage line
-	var usage = cmd.UsageLine
-	for p := cmd.parent; p != nil; p = p.parent {
-		usage = p.UsageLine + " " + usage
-	}
-	fmt.Fprintf(os.Stderr, "Usage: %s\n\n", usage)
-
-	// Print long
-	fmt.Fprintln(os.Stderr, strings.TrimSpace(cmd.Long))
-
-	// Print options
-	var opts bytes.Buffer
-	cmd.Flags.SetOutput(&opts)
-	cmd.Flags.PrintDefaults()
-	if len(opts.String()) != 0 {
-		fmt.Fprintln(os.Stderr, "Options:")
-		fmt.Fprintln(os.Stderr, opts)
-	}
-
-	// Print options for the supercommands as well
-	for p := cmd.parent; p != nil; p = p.parent {
-		fmt.Fprintf(os.Stderr, "Options (supercommand '%s'):", p.Name())
-		p.Flags.PrintDefaults()
-	}
-
-	// Print subcommands
-	if cmd.subcmds != nil {
-		fmt.Fprintf(os.Stderr, "\nSubcommands:\n")
-		for _, subcmd := range cmd.subcmds {
-			fmt.Fprintf(os.Stderr, "  %s \t %s\n", subcmd.Name(), subcmd.Short)
-		}
-	}
+func (cmd *Command) Usage() {
+	t := template.Must(template.New("usage").Parse(usageTemplate))
+	t.Execute(os.Stderr, cmd)
 }
 
-func (cmd *Command) Invoke() {
+func (cmd *Command) invoke(args []string) {
 
 }
 
-func (cmd *Command) ParseAndInvoke(args []string) {
-	err := cmd.Flags.Parse(args)
-	if err != nil {
-		cmd.Usage(err.Error())
+func (cmd *Command) OptionsString() string {
+	return "Options go here"
+}
+
+func (cmd *Command) Execute(args []string) {
+	cmd.Flags.Parse(args)
+	subArgs := cmd.Flags.Args()
+
+	if len(subArgs) == 0 {
+		cmd.invoke(subArgs)
 	}
 
-	fArgs := cmd.Flags.Args()
-
-	if cmd.Args != nil {
-		if len(cmd.Args) != len(fArgs) {
-			cmd.Usage("Invalid number of arguments")
-		}
-		for i := range cmd.Args {
-			cmd.Args[i] = fArgs[i]
-		}
-
-		cmd.Invoke()
-		return
-	}
-
-	if len(fArgs) == 0 {
-		cmd.Invoke()
-		return
-	}
-
-	if cmd.subcmds == nil {
-		cmd.Usage("No subcommand defined: " + fArgs[0])
-	}
-
-	name := fArgs[0]
-	for _, subcmd := range cmd.subcmds {
+	name := subArgs[0]
+	for _, subcmd := range cmd.Subcmds {
 		if subcmd.Name() == name {
 			subcmd.parent = cmd
-			subcmd.ParseAndInvoke(fArgs[1:])
+			subcmd.Execute(subArgs[1:])
 			return
 		}
 	}
 
-	cmd.Usage("No subcommand defined: " + fArgs[0])
+	cmd.invoke(subArgs)
 }
 
 func (cmd *Command) MustRegisterSubcommand(subcmd *Command) {
-	if cmd.subcmds == nil {
-		cmd.subcmds = []*Command{subcmd}
+	// Easy if there are no subcommands defined yet.
+	if cmd.Subcmds == nil {
+		cmd.Subcmds = []*Command{subcmd}
 		return
 	}
 
-	for _, c := range cmd.subcmds {
+	// Check for subcommand name collisions.
+	for _, c := range cmd.Subcmds {
 		if c.Name() == subcmd.Name() {
 			panic(fmt.Sprintf("Subcommand %s already defined", subcmd.Name()))
 		}
 	}
 
-	cmd.subcmds = append(cmd.subcmds, subcmd)
+	// If everything went well, register the subcommand.
+	cmd.Subcmds = append(cmd.Subcmds, subcmd)
 }
